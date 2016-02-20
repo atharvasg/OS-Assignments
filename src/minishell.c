97cb77d32837641690928af58fe131e2a088bc48
Nikhil Bagde <nbagde1@binghamton.edu>
2016-02-19 23:48:11 -0500

    Added Phase 1, If you add something please comment

--- src/minishell.c
+++ src/minishell.c
@@ -1,10 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define BUFFER 1024
 int main(int argc, char * argv[]){
 
+//Phase 1
+	int bufsize = BUFFER;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) *bufsize);
+	char *line;
+
+	if(!buffer){
+		fprintf(stderr, "Allocation Error\n");
+		exit(EXIT_FAILURE);
+	}
+	int c;
+	while(1){
+		c = getchar();
+		if( c == EOF || c=='\n'){
+			buffer[position] = '\0';
+			line =  buffer;
+		}else{
+			buffer[position] =c;
+		}
+	position++;
+		if(position >=bufsize){
+			bufsize += BUFFER;
+			buffer = realloc(buffer, bufsize);
+			if(!buffer){
+				fprintf(stderr,"Allocation Error\n");
+				exit(EXIT_FAILURE);
+			}//inner If
+		}//outer if
+	}//while
+//End of Phase1
 
 	printf("minishell>");
+
 	pid_t pid;
 	int status;
 
